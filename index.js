57c9fc7ed5c5b238afd5e26ce60137af8ec2a221
th4t-gi <juddbrau@gmail.com>
2023-01-10 23:51:13 -0700

    Last commit for heroku to work!

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const path = require('node:path');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] });
 
 if (!fs.existsSync("./" + dbName)) {
+	console.log("generating Database");
 	fs.writeFileSync("./" + dbName, "")
 }
 const db = dev ? new Keyv('sqlite://'+dbName) : new Keyv('sqlite://'+devDatabaseName);
